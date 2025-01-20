86bb1ff65e334242b1f7842b38500af676deee8a
Huy Le <lhqhuy14032001@gmail.com>
2025-01-20 23:35:59 +0700

    add a greeting CI/CD

--- flask_docker.py
+++ flask_docker.py
@@ -3,7 +3,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return "Hello world by LHQH-M3724006"
+	return "Hello world, Hello CI/CD using Jenkins by LHQH-M3724006"
 
 if __name__ == '__main__':
 	app.run(debug="True", host="0.0.0.0")
